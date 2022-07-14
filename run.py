fae373ee6e95391893a12421603133f5dcddadb7
kikiusen <ecvoracle@gmail.com>
2022-07-14 20:14:08 +0100

    Added questionary 1 text file

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -17,7 +17,29 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ci-p3-sheet')
 
 weight = SHEET.worksheet('weight')
+img = SHEET.worksheet('img')
+diff_img = SHEET.worksheet('diff_img')
 
-data = weight.get_all_values()
 
-print(data)
+def exist_username():
+    """
+    Get username if exist in the sheet
+    If not request to create a new one
+    """
+    val = weight.acell('A2').value
+    if val is not None:
+        return "true"
+
+
+def get_create_username():
+    """
+    Get username if exist in the sheet
+    If not request to create a new one
+    """
+    if exist_username() != "true":
+        print("No users registered. Please register your username:")
+    else:
+        print("Register (R) / Access user (A):")
+
+
+get_create_username()
