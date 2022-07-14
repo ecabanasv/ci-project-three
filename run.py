7145159299d7e41351c96426db6d5fdce8971266
kikiusen <ecvoracle@gmail.com>
2022-07-14 20:17:31 +0100

    Added initial functions in run.py: inputRandomizeQuestions and printOptions

--- run.py
+++ run.py
@@ -2,44 +2,83 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
 import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive",
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('ci-p3-sheet')
+SHEET = GSPREAD_CLIENT.open("ci-pp3-sheet")
 
-weight = SHEET.worksheet('weight')
-img = SHEET.worksheet('img')
-diff_img = SHEET.worksheet('diff_img')
+Q1 = SHEET.worksheet("Q1")
 
 
-def exist_username():
-    """
-    Get username if exist in the sheet
-    If not request to create a new one
-    """
-    val = weight.acell('A2').value
-    if val is not None:
-        return "true"
+# def check_user_exist():
+#     """
+#     Get username if exist in the sheet
+#     If not request to create a new one
+#     """
+#     val = Q1.acell("A2").value
+#     if val is not None:
+#         return "true"
+
+# def reg_new_user():
+
+# def access_reg_user():
 
 
-def get_create_username():
-    """
-    Get username if exist in the sheet
-    If not request to create a new one
-    """
-    if exist_username() != "true":
-        print("No users registered. Please register your username:")
-    else:
-        print("Register (R) / Access user (A):")
+# def reg_or_access_user():
+#     """
+#     Get username if exist in the sheet
+#     If not request to create a new one
+#     """
+#     if check_user_exist() != "true":
+#         reg_new_user()
+#     else:
+#         access_reg_user()
 
 
-get_create_username()
+# reg_or_access_user()
+
+def printOptions():
+    print("(A) Never or seldom")
+    print("(B) Sometimes")
+    print("(C) Always or almost always\n")
+
+
+def inputRandomizeQuestions():
+    f = open("q1.txt", "r")
+    list = []
+    answers = []
+    for i in f:
+        list.append(i)
+    random.shuffle(list)
+    for i in range(10):
+        print(list[i])
+        printOptions()
+        a = input("Enter your choice: ")
+        print("\n")
+        answers.append(a)
+
+    for i in range(10):
+        print(answers[i])
+
+
+inputRandomizeQuestions()
+
+
+
+
+
+
+
+
+
+
