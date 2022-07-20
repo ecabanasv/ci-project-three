d126984a4acdc209a5fa4c196ccdf9a3c1ea8a5f
kikiusen <ecvoracle@gmail.com>
2022-07-20 19:35:58 +0100

    Format document with VS Code

--- modules/start.py
+++ modules/start.py
@@ -2,22 +2,6 @@ import time
 
 from .utils import Utils
 from .functions import Functions
-from colorama import Fore
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive",
-]
-
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("ci-pp3-sheet")
-
-Q1 = SHEET.worksheet("Q1")
 
 
 class Start:
@@ -25,42 +9,25 @@ class Start:
     main(): initiates the questionary
     """
 
-    @staticmethod
-    def main():
+    # default constructor
+    def __init__(self):
+        self.sum_points = 0
+        self.user_answers = []
+        self.list_questions = Functions.shuffle_questions()
+        Utils.cls()
+
+    def main(self):
         """
         Function that open gspread of questionary
         random choose 10 questions of file text
         gather all input question choice
         and calculate the total and result
         """
-        user_answers = []
-        list_questions = []
-        sum_points = 0
-        Utils.cls()
         Functions.print_intro()
         time.sleep(10)
         Utils.cls()
-        list_questions = Functions.shuffle_questions()
-
-        for i in range(10):
-            print(Fore.YELLOW + list_questions[i])
-            Functions.print_options()
-            while True:
-                user_input = input(Fore.WHITE + "Enter your choice: ")
-                if user_input.upper() in ["A", "B", "C"]:
-                    break
-                print(Fore.RED + "Please enter characters A, B or C only")
-            sum_points = Functions.questionary_total(user_input, sum_points)
-            print("\n")
-            user_answers.append(user_input.upper())
-            Utils.cls()
-        user_answers.append(sum_points)
-        Functions.questionary_result(sum_points)
-        Q1.append_row(user_answers)
-        values_list = Q1.col_values(11, value_render_option="UNFORMATTED_VALUE")
-        values_list.remove("result")
-        if len(values_list) > 1:
-            print(Fore.MAGENTA + "\nTOTAL:\n")
-            print(Fore.WHITE + "  There are " + str(len(values_list)) + " participants in total",
-            "with an average of " + str(round(sum(values_list)/len(values_list), 2)) + " points.\n"
-            )
+        Functions.run_questionary(
+            self.list_questions, self.user_answers, self.sum_points
+        )
+        Functions.print_user_result(self.sum_points)
+        Functions.print_total_result()
